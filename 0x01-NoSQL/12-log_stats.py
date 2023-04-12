c28ac1d2f1f93cc87dda94494cc60c9e67144e05
Stellacandy <stella.me90@gmail.com>
2023-04-12 12:07:47 +0000

    Task 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,39 +1,36 @@
 #!/usr/bin/env python3
-"""
-Provide some stats about Nginx logs stored in MongoDB
-Database: logs, Collection: nginx, Display same as example
-first line: x logs, x number of documents in this collection
-second line: Methods
-5 lines with method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-one line with method=GET, path=/status
-"""
+
+
 from pymongo import MongoClient
 
-
-METHODS = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-
-def log_stats(mongo_collection, option=None):
-    """
-    Prototype: def log_stats(mongo_collection, option=None):
-    Provide some stats about Nginx logs stored in MongoDB
-    """
-    items = {}
-    if option:
-        value = mongo_collection.count_documents(
-            {"method": {"$regex": option}})
-        print(f"\tmethod {option}: {value}")
-        return
-
-    result = mongo_collection.count_documents(items)
-    print(f"{result} logs")
-    print("Methods:")
-    for method in METHODS:
-        log_stats(nginx_collection, method)
-    status_check = mongo_collection.count_documents({"path": "/status"})
-    print(f"{status_check} status check")
+'''Python script that provides some stats about Nginx
+logs stored in MongoDB'''
 
 
-if __name__ == "__main__":
-    nginx_collection = MongoClient('mongodb://127.0.0.1:27017').logs.nginx
-    log_stats(nginx_collection)
+def get_stats(collection, obj):
+    '''Returns the number of documents in a collection'''
+    return collection.count_documents(obj)
+
+
+def print_stats():
+    '''Prints stats'''
+
+    con = MongoClient('mongodb://localhost:27017')
+    db = con.logs
+    collection = db.nginx
+
+    print(f'{collection.estimated_document_count()} logs')
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    print('Methods:')
+
+    for req in methods:
+        print('\tmethods {}: {}'.format(req,
+              get_stats(collection, {'method': req})))
+
+    print('{} status check'.format(get_stats(collection,
+          {'method': 'GET', 'path': '/status'})))
+
+
+if __name__ == '__main__':
+    print_stats()
